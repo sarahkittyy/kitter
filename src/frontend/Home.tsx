9582b39c90e67b463ab6d6bcdf2ac4dcfb28531e
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-01 01:13:52 -0400

    added drawer manipulation

--- src/frontend/Home.tsx
+++ src/frontend/Home.tsx
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
 import TopBar from './Components/TopBar';
+import ToggleDrawer from './Components/ToggleDrawer';
+
+export interface HomeState
+{
+	drawer: {
+		left: boolean;
+		right: boolean;
+		top: boolean;
+		bottom: boolean;
+	}
+};
 
 /**
  * @brief Homepage component.
  */
-export default class Home extends Component<{}, {}>
+export default class Home extends Component<{}, HomeState>
 {
 	public constructor(props: React.Props<{}>)
 	{
 		super(props);
+		
+		this.state = {
+			drawer: {
+				left: false,
+				right: false,
+				top: false,
+				bottom: false
+			}
+		};
 	}
 	
 	public componentDidMount()
@@ -16,11 +36,37 @@ export default class Home extends Component<{}, {}>
 		document.title = "kitter";
 	}
 	
+	public toggleDrawer(side: 'left' | 'right' | 'top' | 'bottom', open?: boolean): void {
+		if(open === undefined)
+		{
+			this.setState({
+				...this.state,
+				drawer: {
+					...this.state.drawer,
+					[side]: !this.state.drawer[side]
+				}
+			});
+		}
+		else
+		{
+			this.setState({
+				...this.state,
+				drawer: {
+					...this.state.drawer,
+					[side]: open
+				}
+			})
+		}
+	}
+	
 	public render()
 	{
 		return (
 			<>
-				<TopBar />
+				<TopBar toggleDrawer={(side, open) => this.toggleDrawer(side, open)}/>
+				<ToggleDrawer side="left" toggleDrawer={(open?: boolean) => this.toggleDrawer('left', open)} open={this.state.drawer.left}>
+					<h1>owo</h1>
+				</ToggleDrawer>
 			</>
 		);
 	}
