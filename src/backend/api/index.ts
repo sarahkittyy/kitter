cf409e5e465a1ff30ce3bc9174f166dbab034944
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-01 18:34:45 -0400

    testing endpoints

--- src/backend/api/index.ts
+++ src/backend/api/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
-import jwt from 'json-web-token';
+import secure from './auth/secure';
 
 // Main api endpoint (/api)
 const api = express.Router();
+
+api.get('/', [secure], (req: express.Request, res: express.Response) => {
+	res.send('o3o');
+});
+
+api.all('/*', (req, res) => {
+	res.status(404).send('No API endpoint /api' + req.url);
+});
 
 export default api;
