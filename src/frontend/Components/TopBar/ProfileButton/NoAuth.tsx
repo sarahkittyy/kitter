70bf074dd047892aa104495f70994515fac0fb8b
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-04 09:57:28 -0400

    implemented login button

--- src/frontend/Components/TopBar/ProfileButton/NoAuth.tsx
+++ src/frontend/Components/TopBar/ProfileButton/NoAuth.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Typography, Button } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
 
 export interface Props
 {
 	
 };
 
+export interface State
+{
+	redirectToLogin: boolean;
+};
+
 /**
  * @brief Component displayed by ProfileButton when we aren't logged in.
  */
-export default function NoAuth(props: Props): JSX.Element
+export default class NoAuth extends Component<Props, State>
 {
-	return (
-		<Button>
-			<Typography 
-				variant="h6"
-				component="p"
-				style={{textTransform: 'none', width: '100%', height: '100%', color: 'white'}}>
-				- login -
-			</Typography>
-		</Button>
-	);
+	public constructor(props: Props)
+	{
+		super(props);
+		
+		this.state = {
+			redirectToLogin: false
+		};
+	}
+	
+	public render()
+	{
+		return (
+		<>
+			<Button onClick={() => this.setState({...this.state, redirectToLogin: true})}>
+				<Typography 
+					variant="h6"
+					component="p"
+					style={{textTransform: 'none', width: '100%', height: '100%', color: 'white'}}>
+					- login -
+				</Typography>
+			</Button>
+			{this.state.redirectToLogin ? <Redirect to="/login"/> : ''}
+		</>
+		);	
+	}
 }
