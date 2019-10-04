2a0c5b97cc839e9ba5273e8253e1c0564d1d5c84
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-04 09:57:37 -0400

    added login page route

---
+++ src/frontend/Login.tsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {} from '@material-ui/core';
+
+export interface Props
+{
+	
+};
+
+export interface State
+{
+	
+};
+
+/**
+ * @brief The main login page.
+ */
+export default class Login extends Component<Props, State>
+{
+	public constructor(props: Props)
+	{
+		super(props);
+		
+		this.state = {
+			
+		};
+	}
+	
+	public render()
+	{
+		return (
+			<h1>hi</h1>
+		);
+	}
+};
--- src/frontend/Routes.tsx
+++ src/frontend/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route, Link, Redirect} from 'react-router-dom';
 import Home from './Home';
+import Login from './Login';
 
 /**
  * @brief All app routes.
@@ -9,5 +10,6 @@ export default (
 	<Switch>
 		<Route exact path="/" component={() => <Redirect to="/home" />} />
 		<Route path="/home" component={Home} />
+		<Route path="/login" component={Login} />
 	</Switch>	
 );
