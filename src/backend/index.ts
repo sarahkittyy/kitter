8b4d91f42f60624d1abe0cc43d7c00a3671fe956
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-04 11:51:37 -0400

    removed /assets endpoint

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -14,15 +14,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Static file transfer
-app.use('/public', express.static('public'));
-app.use('/assets', express.static('public/assets'));
+app.use('/public', express.static(appRoot.resolve('public')));
 
 // API Endpoint
 app.use('/api', api);
 
 // Favicon
 app.get('/favicon.ico', (req, res) => {
-	res.sendFile(appRoot.resolve('./assets/favicon.ico'));
+	res.sendFile(appRoot.resolve('public/assets/favicon.ico'));
 });
 
 // Send the html for all unhandled requests
